ca5a49f265067986eb8d46f68bb98e244e4b0055
Rajat Maharjan <mhzrajat123@gmail.com>
2025-09-18 20:43:54 +0545

    Third Commit

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 # Gender -> 1 is Female and 0 is Male
 # Churn -> 1 is Yes and 0 is No
 # Scaler is exported as scaler.pkl
+# Best model is exported as model.pkl
+# Order of the X -> 'Age', 'Gender', 'Tenure', 'MonthlyCharges'
+
